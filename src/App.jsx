9e648a2cd0099601f4250fcb83f16a0bcd7181b6
Peter Osoo <posoo617@gmail.com>
2023-08-30 17:42:42 +0300

    render page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import "./App.css"
+
+import Form from "./components/Form"
+
 const App = () => {
-	return <div>Recipe App</div>
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="App-title">Recipe Search</h1>
+			</header>
+			<Form />
+		</div>
+	)
 }
 
 export default App
